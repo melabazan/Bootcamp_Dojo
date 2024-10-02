ea85b716940d76458b6f6aa86538b635d1cab6c4
mela <melbazan97@fpuna.edu.py>
2024-10-01 21:56:34 -0400

    Mi informacion

---
+++ MiInformacion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class MiInformacion{
+
+    public static void main(String[] args) {
+        System.out.println("Hola, mi nombre es Melani Bazán.");
+        System.out.println("Tengo 27 años de edad.");
+        System.out.println("Soy de Paraguay y vivo en la ciudad de Asunción.");
+    }    
+    
+}
